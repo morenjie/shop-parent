d18b5e38918537382a6c25a46ac2fd29e2862595
coisini <18721966209@163.com>
2022-05-19 23:27:34 +0800

    修改、删除购物车信息

--- shop-cart-web/src/main/java/com/qf/controller/CartWebApplication.java
+++ shop-cart-web/src/main/java/com/qf/controller/CartWebApplication.java
@@ -1,5 +1,6 @@
 package com.qf.controller;
 
+import com.qf.feign.CartFeign;
 import com.qf.feign.ItemFeign;
 import com.qf.pojo.TbItem;
 import com.qf.utils.CookieUtils;
@@ -21,6 +22,8 @@ import java.util.*;
 public class CartWebApplication {
     @Autowired
     ItemFeign itemFeign;
+    @Autowired
+    CartFeign cartFeign;
 
     /**
      * 添加商品到购物车
@@ -35,9 +38,10 @@ public class CartWebApplication {
     public Result addItem(@RequestParam("itemId") Long itemId, @RequestParam("userId") Long userId, @RequestParam(value = "num", defaultValue = "1") Integer num, HttpServletRequest request, HttpServletResponse response) {
         //判断用户是否登录 如果没有登录 那么购物车信息是保存在cookie端；如果用户登录了，购物车信息是保存在redis缓存中
         try {
+            //如果用户ID为空就是没有登录
             if (userId == null) {
-                //没有登录
-                //需要将购物车信息查询出来Map<itemId,TbItem>
+                //需要将购物车信息查询出来Map<itemId,TbItem>（数据模型）  TbItem：商品对象
+                //需要从cookie里面找
                 Map<String, TbItem> cart = getCartFromCookie(request);
                 //将商品添加到购物车
                 addItemToCart(cart, itemId, num);
@@ -65,7 +69,7 @@ public class CartWebApplication {
         //根据商品id查询购物车信息
         TbItem tbItem = cart.get(itemId.toString());
         if (tbItem != null) {
-            //之前的购物车存在同样的商品信息 我们只需要修改商品信息的购买数量即可
+            //之前的购物车存在同样的商品信息 我们只需要修改商品信息的购买数量即可 此时的num描述的是商品的数量
             tbItem.setNum(tbItem.getNum() + num);
         } else {
             //说明购物车里面不存在这个商品，这个商品是第一次添加
@@ -84,7 +88,7 @@ public class CartWebApplication {
             Map<String, TbItem> map = JsonUtils.jsonToMap(cart_cookie_key, TbItem.class);
             return map;
         } else {
-            //购物车不存在，第一次添加商品到购物车,那么我就初始化一个空的购物车
+            //购物车不存在，第一次添加商品到购物车,那么 就初始化一个空的购物车
             return new HashMap<String, TbItem>();
         }
     }
@@ -96,7 +100,7 @@ public class CartWebApplication {
             if (userId == null) {
                 //没有登录的
                 Map<String, TbItem> cart = getCartFromCookie(request);
-                List<TbItem>tbItemList = new ArrayList<>();
+                List<TbItem> tbItemList = new ArrayList<>();
                 //获取cart中的所有value值
                 Set<String> keys = cart.keySet();   //获取所有key的集合
                 for (String key : keys) {
@@ -112,6 +116,71 @@ public class CartWebApplication {
             e.printStackTrace();
             return Result.error("展示购物车信息失败");
         }
+    }
+
+    /**
+     * 修改购物车信息
+     *
+     * @param itemId
+     * @param userId
+     * @param num
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping("updateItemNum")
+    public Result updateItemNum(
+            @RequestParam("itemId") Long itemId,
+            @RequestParam("userId") Long userId,
+            @RequestParam("num") Integer num,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        //首先判断用户是否登录
+        try {
+            if (userId == null) {
+                //没有登录，从cookie中获取购物车信息进行修改
+                Map<String, TbItem> cart = getCartFromCookie(request);
+                //修改购物车中对应的商品的数量信息
+                TbItem tbItem = cart.get(itemId.toString());
+                tbItem.setNum(num);
+                cart.put(itemId.toString(), tbItem);
+                //重新的将购物车写入cookie中
+                addCartToCookie(request, response, cart);
+            } else {
+                //已经登录，从缓存中获取购物车信息进行修改
+
+            }
+            return Result.ok();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.error("修改购物车信息失败");
+        }
+    }
+
+    @RequestMapping("deleteItemFromCart")
+    public Result deleteItemFromCart(
+            @RequestParam("itemId") Long itemId,
+            @RequestParam("userId") Long userId,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        //用户没有登录
+        try {
+            if (userId == null) {
+                //将购物车信息从cookie中获取到，然后转换成map
+                Map<String, TbItem> cart = getCartFromCookie(request);
+                //再通过map的key值进行删除；
+                cart.remove(itemId.toString());
+                // 删除之后，需要把删除后的购物车信息重新写入到cookie中
+                addCartToCookie(request, response, cart);
+            } else {
+                //用户已登录
+            }
+            return Result.ok();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.error("删除购物车信息失败");
+        }
 
     }
+
 }
